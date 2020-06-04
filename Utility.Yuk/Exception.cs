b2f8a6df88a16574c9688b04f35eefc8e3c4c1b9
Vinoth Sabanadesan <vinoth15@gmail.com>
2020-06-04 18:00:39 -0400

    Added utility function

---
+++ Utility.Yuk/Exception.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Utility.Yuk
+{
+    public class Exception
+    {
+        public static void CheckIsIntializedOrThrow(params Object[] args)
+        {
+            int i = 1;
+            foreach (Object arg in args)
+            {
+                if (arg == null)
+                {
+                    throw new System.Exception("Please initialize parameter: " + i);
+                }
+
+                i++;
+            }
+        }
+    }
+}
