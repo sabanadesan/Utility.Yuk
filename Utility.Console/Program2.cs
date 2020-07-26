0d23326afe28af91a353ca8a36ee4eb8fad9690f
Vinoth Sabanadesan <vinoth15@gmail.com>
2020-07-26 14:22:56 -0400

    Added Pkg

---
+++ Utility.Console/Program2.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+
+using Utility.Pkg;
+
+namespace Utility.Console
+{
+    class Program2
+    {
+        static void Main(string[] args)
+        {
+            
+
+            var test1FilePath = AppDomain.CurrentDomain.BaseDirectory + "PackageTest\\test\\test_1.txt";
+            var test2FilePath = AppDomain.CurrentDomain.BaseDirectory + "PackageTest\\test\\test_2.txt";
+
+            var packageFilePath = AppDomain.CurrentDomain.BaseDirectory + "PackageTest\\test.zip";
+
+            var filePackage = new FilePackage
+            {
+                FilePath = packageFilePath,
+                ContentFilePathList = new List<string>
+                {
+                    test1FilePath, test2FilePath
+                }
+            };
+
+            //// 2. ZIP IN MEMORY. Create zipped byte array from string
+            var filePackageWriter = new FilePackageWriter(filePackage);
+            var zippedBuffer = filePackageWriter.GeneratePackageMemory();
+
+            var filePackageReader = new FilePackageReader();
+            var filenameFileContentDictionary = filePackageReader.GetFilenameFileContentDictionaryMemory(zippedBuffer);
+
+            foreach (var keyValuePair in filenameFileContentDictionary)
+            {
+                System.Console.WriteLine("Filename: " + keyValuePair.Key);
+                System.Console.WriteLine("Content: " + keyValuePair.Value);
+            }
+
+            //File.WriteAllBytes(packageFilePath, zippedBuffer);
+        }
+
+
+        static void Main2(string[] args)
+        {
+            var packageFilePath = AppDomain.CurrentDomain.BaseDirectory + "PackageTest\\test.zip";
+            var rawFileStream = File.OpenRead(packageFilePath);
+            byte[] zippedtoTextBuffer = new byte[rawFileStream.Length];
+            rawFileStream.Read(zippedtoTextBuffer, 0, (int)rawFileStream.Length);
+
+            var filePackageReader = new FilePackageReader();
+            var filenameFileContentDictionary = filePackageReader.GetFilenameFileContentDictionaryMemory(zippedtoTextBuffer);
+
+            foreach (var keyValuePair in filenameFileContentDictionary)
+            {
+                System.Console.WriteLine("Filename: " + keyValuePair.Key);
+                System.Console.WriteLine("Content: " + keyValuePair.Value);
+            }
+        }
+
+        static void Main0(string[] args)
+        {
+            var packageFilePath = AppDomain.CurrentDomain.BaseDirectory + "PackageTest\\test.zip";
+
+
+            var test1FilePath = AppDomain.CurrentDomain.BaseDirectory + "PackageTest\\test\\test_1.txt";
+            var test2FilePath = AppDomain.CurrentDomain.BaseDirectory + "PackageTest\\test\\test_2.txt";
+
+            var filePackage = new FilePackage
+            {
+                FilePath = packageFilePath,
+                ContentFilePathList = new List<string>
+                {
+                    test1FilePath, test2FilePath
+                }
+            };
+
+            var filePackageWriter = new FilePackageWriter(filePackage);
+            filePackageWriter.GeneratePackage(false);
+
+            var filePackageReader = new FilePackageReader();
+            var filenameFileContentDictionary = filePackageReader.GetFilenameFileContentDictionary(packageFilePath);
+
+            foreach (var keyValuePair in filenameFileContentDictionary)
+            {
+                System.Console.WriteLine("Filename: " + keyValuePair.Key);
+                System.Console.WriteLine("Content: " + keyValuePair.Value);
+            }
+        }
+    }
+}
---
+++ Utility.Pkg/FilePackageWriter.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using System.IO.Compression;
+
+namespace Utility.Pkg
+{
+    public class FilePackageWriter
+    {
+        private readonly string _filepath;
+        private readonly IEnumerable<string> _contentFilePathList;
+        private string _tempDirectoryPath;
+
+        public FilePackageWriter(FilePackage filePackage)
+        {
+            _filepath = filePackage.FilePath;
+            _contentFilePathList = filePackage.ContentFilePathList;
+        }
+
+        public byte[] GeneratePackageMemory()
+        {
+            using (var memoryStream = new MemoryStream())
+            {
+                using (var zipArchive = new ZipArchive(memoryStream, ZipArchiveMode.Create, true))
+                {
+                    foreach (var filePath in _contentFilePathList)
+                    {
+                        // Copy every content file into the temp directory we created before
+                        var filePathInfo = new FileInfo(filePath);
+                        if (filePathInfo.Exists)
+                        {
+                            var textBuffer = File.ReadAllText(filePathInfo.FullName);
+
+                            var demoFile = zipArchive.CreateEntry(filePathInfo.Name);
+
+                            using (var entryStream = demoFile.Open())
+                            {
+                                using (var streamWriter = new StreamWriter(entryStream))
+                                {
+                                    streamWriter.Write(textBuffer);
+                                }
+                            }
+                        }
+                        else
+                        {
+                            throw new FileNotFoundException("File path " + filePath + " doesn't exist!");
+                        }
+                    }
+                }
+
+                return memoryStream.ToArray();
+            }
+        }
+
+        public void GeneratePackage(bool deleteContents)
+        {
+            try
+            {
+                string parentDirectoryPath = null;
+                string filename = null;
+
+                var fileInfo = new FileInfo(_filepath);
+
+                // Get the parent directory path of the package file and if the package file already exists delete it
+                if (fileInfo.Exists)
+                {
+                    filename = fileInfo.Name;
+
+                    var parentDirectoryInfo = fileInfo.Directory;
+                    if (parentDirectoryInfo != null)
+                    {
+                        parentDirectoryPath = parentDirectoryInfo.FullName;
+                    }
+                    else
+                    {
+                        throw new NullReferenceException("Parent directory info was null!");
+                    }
+
+                    File.Delete(_filepath);
+                }
+                else
+                {
+                    var lastIndexOfFileSeperator = _filepath.LastIndexOf("\\", StringComparison.Ordinal);
+                    if (lastIndexOfFileSeperator != -1)
+                    {
+                        parentDirectoryPath = _filepath.Substring(0, lastIndexOfFileSeperator);
+                        filename = _filepath.Substring(lastIndexOfFileSeperator + 1, _filepath.Length - (lastIndexOfFileSeperator + 1));
+                    }
+                    else
+                    {
+                        throw new Exception("The input file path '" + _filepath +
+                                            "' does not contain any file seperators.");
+                    }
+                }
+
+                // Create a temp directory for our package
+                _tempDirectoryPath = parentDirectoryPath + "\\" + filename + "_temp";
+                if (Directory.Exists(_tempDirectoryPath))
+                {
+                    Directory.Delete(_tempDirectoryPath, true);
+                }
+
+                Directory.CreateDirectory(_tempDirectoryPath);
+                foreach (var filePath in _contentFilePathList)
+                {
+                    // Copy every content file into the temp directory we created before
+                    var filePathInfo = new FileInfo(filePath);
+                    if (filePathInfo.Exists)
+                    {
+                        File.Copy(filePathInfo.FullName, _tempDirectoryPath + "\\" + filePathInfo.Name);
+                    }
+                    else
+                    {
+                        throw new FileNotFoundException("File path " + filePath + " doesn't exist!");
+                    }
+                }
+                // Generate the ZIP from the temp directory
+                ZipFile.CreateFromDirectory(_tempDirectoryPath, _filepath);
+            }
+            catch (Exception e)
+            {
+                var errorMessage = "An error occured while generating the package. " + e.Message;
+                throw new Exception(errorMessage);
+            }
+            finally
+            {
+                // Clear the temp directory and the content files
+                if (Directory.Exists(_tempDirectoryPath))
+                {
+                    Directory.Delete(_tempDirectoryPath, true);
+                }
+
+                if (deleteContents)
+                {
+                    foreach (var filePath in _contentFilePathList)
+                    {
+                        if (File.Exists(filePath))
+                        {
+                            File.Delete(filePath);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
